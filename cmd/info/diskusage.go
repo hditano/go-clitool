3d49466624143812d452251c39797e7c05209b7f
hditano <hditano@gmail.com>
2024-09-01 18:30:03 +0200

    added diskusage func

--- cmd/info/diskusage.go
+++ cmd/info/diskusage.go
@@ -5,8 +5,11 @@ package info
 
 import (
 	"fmt"
+	"github/hditano/clitool/cmd/utils"
+	"log"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/sys/unix"
 )
 
 // diskusageCmd represents the diskusage command
@@ -15,7 +18,21 @@ var diskusageCmd = &cobra.Command{
 	Short: "A command that will output Disk Usage information",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("diskusage called")
+		fmt.Println("Disk Stats")
+		var c unix.Statfs_t
+		err := unix.Statfs("/", &c)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		total := c.Blocks * uint64(c.Bsize) / utils.GB
+		free := c.Bfree * uint64(c.Bsize) / utils.GB
+		used := total - free
+
+		fmt.Printf("Total Disk Space: %d GB\n", total)
+		fmt.Printf("Free Disk Space: %d GB\n", free)
+		fmt.Printf("Used Disk Space: %d GB\n", used)
 	},
 }
 
